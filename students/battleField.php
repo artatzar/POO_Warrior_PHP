57103b9a54b5041bd501045a2c1ccf088981301b
artatzar <ahmed_slimani@live.fr>
2018-02-27 14:49:52 +0100

    fin du tp

--- students/battleField.php
+++ students/battleField.php
@@ -7,5 +7,24 @@
   class BattleField extends BaseBattleField
   {
 
+        static function createMyWarrior () {
+          $guerrier = new PokemonWarrior($GLOBALS['warriorID']);
+          $guerrier->SetImageUrl("https://assets.pokemon.com/assets/cms2/img/pokedex/full/729.png");
+          $arme = new Weapon('Fusil', 100);
+          $arme-> SetImageUrl("http://www.essai-armes.fr/wp-content/uploads/2017/10/HK-MR223-bo%C3%AEtier-de-culasse_petite.jpg");
+          $guerrier->SetWeapon($arme);
+          $guerrier->saveNew();
 
   }
+        static function createOtherWarrior(){
+          $guerrier= new MarvelWarrior ($id='Toto');
+          $guerrier->SetImageUrl ("https://www.woolworths.com.au/shop/discover/heroes");
+          $arme = new Weapon('Lance', 50);
+          $arme-> SetImageUrl("https://i.annihil.us/u/prod/marvel//universe3zx/images/9/91/Gungrir.jpg");
+          $guerrier->SetWeapon($arme);
+          $guerrier->saveNew();
+      }
+  }
+
+
+
--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,93 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'POLIK';
 
 // Définissez vos class de combattants
-abstract class Warrior extends LocalWarrior {
+abstract class Warrior extends LocalWarrior
+{
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
 
+    public function __construct($id){
+        $this->id=$id;
+        $this->speed=30;
+        $this->life=100;
+        $this ->shield=20;
+
+    }
+
+    public function SetWeapon ($weapon)
+    {
+        $this->weapon=$weapon;
+    }
+
+    public function SetImageUrl($url){
+        $this->imageUrl=$url;
+
+    }
 };
+class StartrekWarrior extends Warrior {
+    public $mentalPower;
+
+    public function power(){
+        return $this->mentalPower;
+
+    }
+    public function __construct($id){
+        Warrior::__construct ($id);
+        $this->mentalPower=$mentalPower = 8;
+
+    }
+
+}
+
+class MarvelWarrior extends Warrior {
+    public $superPower;
+
+    public function power(){
+        return $this->superPower;
+    }
+    public function __construct($id){
+        Warrior::__construct ($id);
+        $this->superPower=$superPower = 100;
+
+    }
+
+}
+
+class PokemonWarrior extends Warrior {
+    public $level;
+
+    public function power(){
+        return $this->level;
+    }
+
+    public function __construct($id){
+            Warrior::__construct ($id);
+            $this->level=$level = 1;
+
+}}
+
+class Weapon extends warrior {
+    public $id;
+    public $strength;
+    public $imageUrl;
+
+    public function __construct($id, $strength){
+        $this->id=$id;
+        $this->strength=$strength;
+
+
+    }
+    public function SetImageUrl($url){
+        $this->imageUrl=$url;
+    }
+
+}
+
